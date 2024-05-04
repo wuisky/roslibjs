25d78feccccd5197a224cecc6e0b10701a356c01
WU MENGHUNG <wu.menghung@jp.panasonic.com>
2024-05-04 11:33:10 +0900

    update src/tf/index.js to export TF2Client

--- src/tf/index.js
+++ src/tf/index.js
@@ -2,7 +2,8 @@ var Ros = require('../core/Ros');
 var mixin = require('../mixin');
 
 var tf = module.exports = {
-    TFClient: require('./TFClient')
+    TFClient: require('./TFClient'),
+    TF2Client: require('./TF2Client')
 };
 
-mixin(Ros, ['TFClient'], tf);
+mixin(Ros, ['TFClient','TF2Client'], tf);
